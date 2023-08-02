a308f435ac8603f6baf8cfdee2545ddb6ce2fa22
natalikoyani <natalikagami@gmail.com>
2023-08-02 21:58:47 +0200

    2
    Made general task and applied SlimSelect lib

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,12 +2,15 @@ import axios from "axios";
 
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const END_POINT = '/breeds';
+const SEARCH = '/images/search';
 const API_KEY = "live_Lz9WDGevkw0t245016RCIX5YUMv5hqO3i1fnW8DcH4wxPk7vOVLEkpYHFQJwBDtQ";
 
 axios.defaults.headers.common["x-api-key"] = API_KEY;
 
 export function fetchBreeds() {
-    return axios(`${BASE_URL}${END_POINT}`)
-    .then(response => console.log(response))
-    .catch(err => console.error(err))
+    return axios(`${BASE_URL}${END_POINT}`);
+}
+
+export function fetchCatByBreed(breedId) {
+    return axios(`${BASE_URL}${SEARCH}?breed_ids=${breedId}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
 const select = document.querySelector('.breed-select');
+let catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+select.hidden = true;
+error.hidden = true;
+
+fetchBreeds()
+    .then(response => {
+        const selectMarkup = response.data.map(elem => `<option value = "${elem.id}">${elem.name}</option>`).join();
+        select.insertAdjacentHTML('beforeend',selectMarkup);
+        new SlimSelect({
+            select: '.breed-select',
+            settings: {
+                allowDeselect: true
+            }
+        })
+        select.hidden = false;
+        loader.hidden = true;
+    })
+    .catch(err => {
+        loader.hidden = true;
+        error.hidden = false;
+    })
+
+    
+
+select.addEventListener('change', chooseBreed);
+
+function chooseBreed(evt) {
+    let breedId = evt.target.value;
+    catInfo.innerHTML = '';
+    error.hidden = true;
+    loader.hidden = false;
+    fetchCatByBreed(breedId)
+    .then(response => {
+        catInfo.innerHTML = `
+            <img
+            src = ${response.data[0].url}
+            alt = ${response.data[0].breeds[0].name}
+            width = '400'/>
+            <h2>${response.data[0].breeds[0].name}<h2/>
+            <p>${response.data[0].breeds[0].description}<p/>
+            <p><span>Temperament:<span/> ${response.data[0].breeds[0].temperament}<p/>
+            `
+            loader.hidden = true;
+    })
+    .catch(err => {
+        loader.hidden = true;
+        error.hidden = false;
+    })    
+}
+
+
+
